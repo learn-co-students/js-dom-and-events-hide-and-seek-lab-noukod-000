d5f453ff2f891d586c9ae0bf7b6e1eccd64aea64
Samuel Cornet <samuel@noukod.com>
2019-02-12 19:35:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  return document.querySelector('div#nested div.target')
+}
+function deepestChild() {
+  return document.querySelector('div#grand-node div div div div')
+}
+function increaseRankBy(n) {
+  let numbers = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i].innerHTML = (parseInt(numbers[i].innerHTML) + n).toString()
+  }
+}
